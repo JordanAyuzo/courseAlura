dcb425c5a5d26a86d9426c155292fd32b716489d
JordanAyuzo <aupj021014@gs.utm.mx>
2024-06-22 15:44:33 -0600

    loops

--- curso1/app.js
+++ curso1/app.js
@@ -1,22 +1,31 @@
 // Declaraciion de variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = 0;
+let intentos= 0;
 
 // Imprimir en consola
-console.log(numeroUsuario);
 
-// Condicional para saber si el numero es igual al numero secreto
-if (numeroUsuario == numeroSecreto) {
-    // la condicion se cumple
-    alert(`Acertaste el numero ${numeroSecreto}`);
-}else{
-    // la condicion no se cumple
-    if (numeroUsuario > numeroSecreto){
-        // el numero es mayor al numero secreto
-        alert(`El numero secreto es menor a ${numeroUsuario}`);
-        
+while (numeroUsuario != numeroSecreto && intentos > 0) {
+    numeroUsuario = parseInt(prompt("Adivina el numero secreto"));
+    console.log(numeroUsuario);
+
+    // Condicional para saber si el numero es igual al numero secreto
+    if (numeroUsuario == numeroSecreto) {
+        // la condicion se cumple
+        alert(`Acertaste el numero ${numeroSecreto} en ${3-intentos} intentos`);
     }else{
-        // el numero es menor al numero secreto
-        alert(`El numero secreto es mayor a ${numeroUsuario}`);
-    }
-}   
+        // la condicion no se cumple
+        if (numeroUsuario > numeroSecreto){
+            // el numero es mayor al numero secreto
+            alert(`El numero secreto es menor a ${numeroUsuario}`);
+
+        }else{
+            // el numero es menor al numero secreto
+            alert(`El numero secreto es mayor a ${numeroUsuario}`);
+        }
+        intentos--;
+    }   
+}
+if (intentos == 0){
+    alert(`Se te acabaron los intentos, el numero secreto era ${numeroSecreto}`);
+}
