df1a8cd0d17665085e5ca4dd9471f1ec7a4b2a19
JordanAyuzo <aupj021014@gs.utm.mx>
2024-06-22 14:53:43 -0600

    Iniciando con JavaScript

--- curso1/app.js
+++ curso1/app.js
@@ -1,1 +1,8 @@
-alert("Hola Mundo!");
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
