5a0b9f215410d21ed64c2a75e21d7311e711616f
JordanAyuzo <aupj021014@gs.utm.mx>
2024-06-22 15:26:18 -0600

    condicionales y concatenacion

--- curso1/app.js
+++ curso1/app.js
@@ -1,8 +1,22 @@
+// Declaraciion de variables
 let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
+// Imprimir en consola
 console.log(numeroUsuario);
 
+// Condicional para saber si el numero es igual al numero secreto
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
-}
+    // la condicion se cumple
+    alert(`Acertaste el numero ${numeroSecreto}`);
+}else{
+    // la condicion no se cumple
+    if (numeroUsuario > numeroSecreto){
+        // el numero es mayor al numero secreto
+        alert(`El numero secreto es menor a ${numeroUsuario}`);
+        
+    }else{
+        // el numero es menor al numero secreto
+        alert(`El numero secreto es mayor a ${numeroUsuario}`);
+    }
+}   
