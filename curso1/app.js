c145c8a7bc265ab7b864a330695fced6587aad8f
JordanAyuzo <aupj021014@gs.utm.mx>
2024-06-22 16:08:15 -0600

    buenas practicas en programacion

--- curso1/app.js
+++ curso1/app.js
@@ -1,18 +1,20 @@
 // Declaraciion de variables
-let numeroSecreto = 6;
+// genera un numero aleatorio entre 1 y 10 con Math.random() * 10
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 let numeroUsuario = 0;
-let intentos= 0;
+let intentos= 1;
+let maximoIntentos = 5;
 
 // Imprimir en consola
 
-while (numeroUsuario != numeroSecreto && intentos > 0) {
+while (numeroUsuario != numeroSecreto ) {
     numeroUsuario = parseInt(prompt("Adivina el numero secreto"));
     console.log(numeroUsuario);
 
     // Condicional para saber si el numero es igual al numero secreto
     if (numeroUsuario == numeroSecreto) {
         // la condicion se cumple
-        alert(`Acertaste el numero ${numeroSecreto} en ${3-intentos} intentos`);
+        alert(`Acertaste el numero ${numeroSecreto} en ${intentos} ${intentos> 1 ? "intentos" : "intento"}`);
     }else{
         // la condicion no se cumple
         if (numeroUsuario > numeroSecreto){
@@ -23,9 +25,11 @@ while (numeroUsuario != numeroSecreto && intentos > 0) {
             // el numero es menor al numero secreto
             alert(`El numero secreto es mayor a ${numeroUsuario}`);
         }
-        intentos--;
-    }   
-}
-if (intentos == 0){
-    alert(`Se te acabaron los intentos, el numero secreto era ${numeroSecreto}`);
+        if (intentos == maximoIntentos){
+            // se acabaron los intentos
+            alert(`Se acabaron los intentos, el numero secreto era ${numeroSecreto}`);
+            break;
+        }
+    }
+    intentos++;   
 }
